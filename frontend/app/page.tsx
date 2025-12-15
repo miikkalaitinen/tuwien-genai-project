8c97f48dae28ee6ce61729f8983044e4e53ba388
miikkalaitinen <38249083+miikkalaitinen@users.noreply.github.com>
2025-12-15 12:28:25 +0100

    basic project structure

---
+++ frontend/app/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+
+
+  
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+        <h1 className="text-4xl font-bold">Welcome to GenAI App</h1>
+      </div>
+    </main>
+  );
+}
